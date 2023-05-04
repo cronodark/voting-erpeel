ae85ad5653ed3438be3566e51b81c3110e70e1e3
Arya <yaar81@gmail.com>
2023-05-04 16:09:30 +0700

    SiswaController and AuthController Optimized

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -15,41 +15,84 @@ class AuthController extends Controller
     {
         return view('auth.index');
     }
-
     public function login(Request $request)
     {
         $credentials = $request->validate([
             'username' => ['required'],
             'password' => ['required'],
         ]);
-        // dd($credentials);
+
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
             $user = Auth::user();
             $siswa = Siswa::where('user_id', $user->id)->first();
-            // dd($siswa);
+
             if ($user->role == 1) {
-                $checkVote = Vote::where('siswa_id', $siswa->id)->where('kandidat_ketua_id', 1)
-                    ->orWhere('kandidat_ketua_id', 2)->first();
+                $checkVote = Vote::where('siswa_id', $siswa->id)
+                    ->where(function ($query) {
+                        $query->where('kandidat_ketua_id', 1)
+                            ->orWhere('kandidat_ketua_id', 2);
+                    })
+                    ->where(function ($query) {
+                        $query->where('kandidat_wakil_id', 1)
+                            ->orWhere('kandidat_wakil_id', 2);
+                    })
+                    ->first();
+
                 if ($checkVote == null) {
-                    return redirect()->route('user.murid.index')->with('error', 'You have already voted.');
+                    return redirect()->route('user.murid.index');
+                } else {
+                    Auth::logout();
+                    return redirect()->route('auth.index')->with('error', 'You have already voted.');
                 }
-                return redirect()->route('user.murid.index');
             } elseif ($user->role == 2) {
                 return redirect()->route('user.admin.index');
             }
+            return redirect()->route('auth.index')->with('error', 'Username atau password salah!');
         }
-
-        return redirect()->route('auth.index')->with('error', 'Username atau password salah!');
     }
+
+    // public function login(Request $request)
+    // {
+    //     $credentials = $request->validate([
+    //         'username' => ['required'],
+    //         'password' => ['required'],
+    //     ]);
+    //     // dd($credentials);
+    //     if (Auth::attempt($credentials)) {
+    //         $request->session()->regenerate();
+    //         $user = Auth::user();
+    //         $siswa = Siswa::where('user_id', $user->id)->first();
+    //         // dd($siswa);
+    //         if ($user->role == 1) {
+    //             $checkVote = Vote::where('siswa_id', $siswa->id)->where('kandidat_ketua_id', 1)
+    //                 ->orWhere('kandidat_ketua_id', 2)->get();
+    //             dd($checkVote);
+    //             if ($checkVote == null) {
+    //                 return redirect()->route('user.murid.index');
+    //             } else {
+    //                 Auth::logout();
+    //                 return redirect()->route('auth.index')->with('error', 'You have already voted.');
+    //             }
+    //             // if ($checkVote->kandidat_ketua_id == 1 or 2 && $checkVote->kandidat_wakil_id == 1 or 2) {
+    //             //     return redirect()->route('auth.index')->with('error', 'You have already voted.');
+    //             // } else if (!$checkVote->kandidat_ketua_id == 1 or 2 && $checkVote->kandidat_wakil_id == 1 or 2) {
+    //             //     return redirect()->route('user.murid.index');
+    //             // }
+    //         } elseif ($user->role == 2) {
+    //             return redirect()->route('user.admin.index');
+    //         }
+    //     }
+
+
 
     public function logout(Request $request)
     {
-        Auth::logout();
-
         $request->session()->invalidate();
 
         $request->session()->regenerateToken();
+        Auth::logout();
+
         return redirect('/');
     }
 }
